2d11875067c365e71e3793ecb3642b26e2f58bfb
osulca <omarsulca@gmail.com>
2020-05-31 18:05:42 -0500

    inicial

---
+++ operaciones.js
@@ -0,0 +1,8 @@
+var x, y;
+x = 5;
+y = 7;
+z = x + y;
+document.getElementById('suma').innerHTML = "el resultado de la suma es: " + z;
+document.getElementById('resta').innerHTML = "el resultado de la resta es: " + (x - y);
+document.getElementById('multiplicacion').innerHTML = "el resultado de la multiplicacion es: " + (x * y);
+document.getElementById("division").innerHTML = 'el resultado de la division es: '  + (x / y);
